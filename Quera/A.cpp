36aed78b2d04f7347434e641e4539184721461ab
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2022-03-31 10:39:35 -0400

    Add files via upload

---
+++ Quera/A.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define endl '\n'
+string s;
+int i;
+void decode(char c)
+{
+	int ans=0;
+	stringstream str;
+	while (isdigit(s[i])&&s[i]!='\0') {
+		str << s[i];
+		i++;
+	}
+	str >> ans;
+	ans-=1;
+	while (ans--)
+		cout << c;
+}
+int main()
+{
+	sync;
+	int t; cin >> t;
+	while (t--) {
+		int key; cin >> key;
+		cin >> s;
+		if (key==1) {
+			int cnt=1;
+			for (int i=1; i<=s.length(); i++) {
+				if (s[i]==s[i-1])
+					cnt++;
+				else {
+					cout << s[i-1];
+					if (cnt>1)
+						cout << cnt;
+					cnt=1;
+				}
+			}
+			cout << endl;
+		}
+		else {
+			char tmp;
+			for (i=0; i<s.length(); i++) {
+				if (isdigit(s[i]))
+					decode(tmp);
+				tmp=s[i];
+				if (isalpha(tmp))
+					cout << tmp;
+			}
+			cout << endl;
+		}
+	}
+}
