972fef19471cebf1c2ba45423d31cad22bad8eea
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2022-04-01 12:24:40 -0400

    Add files via upload

---
+++ Quera/B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define endl '\n'
+int32_t main()
+{
+	sync;
+	string s; cin >> s;
+	map<char, int>x;
+	for (char c : s) x[tolower(c)]++;
+	for (char c : s) {
+		char y=(((x[tolower(c)]*((tolower(c))-'a'))+1)%26)+((islower(c))?97:65);
+		cout << (char)y;
+	}
+}
