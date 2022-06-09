38b9e549a517a783eabc866609c879d3404f24c5
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2022-06-09 05:11:25 -0400

    Update M.cpp

--- Codeforces/M.cpp
+++ Codeforces/M.cpp
@@ -7,20 +7,15 @@ using namespace std;
 #pragma GCC optimize ("unroll-loops")
 #pragma GCC optimize("O0")
 #pragma GCC optimize("O1")
-#define forn(i, n) for (int i = 0; i < int(n); i++)
 const int inf = 1e9 + 7;
  
 bool equals(vector<int>&a, vector<int>&b, int n){
     int dif = inf;
-    forn(i, n){
-        if(b[i] != 0) dif = min(dif, a[i] - b[i]);
-    }
-    if(dif < 0) return false; 
-    if(dif == inf) return true;
-    forn(i, n){
-        if(a[i] - b[i] > dif) return false;
-        if(b[i] != 0 && a[i] - b[i] < dif) return false;
-    }
+    for (int i = 0; i < n; i++) 
+	 if(b[i] != 0) dif = min(dif, a[i] - b[i]);
+    if(dif < 0) return false;
+    for (int i = 0; i < n; i++) 
+	 if(b[i] != 0 && a[i] - b[i] != dif || a[i] - b[i] > dif) return false;
     return true;
 }
  
@@ -28,13 +23,14 @@ void solve(){
     int n;
     cin >> n;
     vector<int>a(n), b(n);
-    forn(i, n) cin >> a[i];
-    forn(i, n) cin >> b[i];
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++) cin >> b[i];
     cout << (equals(a, b, n) ? "YES\n" : "NO\n");
  
 }
  
-int main(){
+int main() {
+    sync;
     int t;
     cin >> t;
     while(t--){
