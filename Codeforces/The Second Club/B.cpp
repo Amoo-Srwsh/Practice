8fdd1ce9b531b7a49471ef87635da8091580fdd4
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2022-08-17 18:08:18 -0400

    Add files via upload

---
+++ Codeforces/The Second Club/B.cpp
@@ -0,0 +1,52 @@
+/* IN THE NAME OF ALLAH */
+
+
+
+/* problem url */
+// https://codeforces.com/contest/1704/problem/B
+
+
+
+#ifdef DEBUG
+#define _GLIBCXX_DEBUG
+#endif
+//#pragma GCC optimize("O3")
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+int main(int argc, char *argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#ifdef DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+    int tst;
+    cin >> tst;
+    while (tst--) {
+	 int n, x; cin >> n >> x;
+	 vector<int> A(n);
+	 for (int i = 0; i < n; i++) {
+              cin >> A[i];
+	 }
+	 int mxn = -1, mnn = -1, ans = -1;
+	 for (int i = 0; i < n; i++) {
+              int xt = ((A[i] - x <= 0) ? 0 : A[i] - x);
+	      int yt = A[i] + x;
+	      if (xt > mxn || yt < mnn) {
+		  ans++;
+		  mnn = xt;
+		  mxn = yt;
+	      }
+	      else {
+		  mxn = min(mxn, yt);
+		  mnn = max(mnn, xt);
+	      }
+	 }
+	 cout << ans << "\n";
+    }
+
+    return 0;
+}
